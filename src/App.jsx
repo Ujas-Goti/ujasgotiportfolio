6557560c04a3275aee1ca26292e18b947b5df084
Ujas Goti <ujaskg18@gmail.com>
2024-07-26 21:16:53 -0700

    Use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import Experience from './components/Experience';
 import NotFound from './components/NotFound';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
@@ -12,8 +15,10 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/experience' element={<Experience />} />
-       
-
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} /> {/* Handle not found routes */}
       </Routes>
       <Footer />
     </HashRouter>
--- src/index.js
+++ src/index.js
@@ -14,23 +14,11 @@ import Projects from './components/Projects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/experience' element={<Experience/>}/>
-        <Route path='/projects' element={<Projects/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-      </Routes>
-    </BrowserRouter>
-);
-ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
